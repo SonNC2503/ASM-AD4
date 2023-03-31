ffbf7d33a44e9b259eec204b56014784d8163f74
Duyhoclaptrinh <duymau147@gmail.com>
2023-03-31 13:33:40 +0700

    indexAdmin file

---
+++ resources/views/Admin/Page/indexAdmin.blade.php
@@ -0,0 +1,43 @@
+@extends('Admin.Layout.master')
+@section('content')
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col-auto">
+                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><a href="{{ route('admin.role.index') }}">List of Roles</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                
+                                <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="{{ route('admin.account.index') }}">List of Accounts</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
