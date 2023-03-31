dda08b1482b7e2241abe05e321890cd37fb1ba65
Duong <duongmabhaf200060@fpt.edu.vn>
2023-03-31 15:52:01 +0700

    list Topic

---
+++ resources/views/Trainer/Page/indexTrainer.blade.php
@@ -0,0 +1,31 @@
+@extends('Trainer.Layout.master')
+@section('content')
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col-auto">
+                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><a href="{{ route('trainer.topic.index') }}">List of Topics</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    </div>
+@endsection
